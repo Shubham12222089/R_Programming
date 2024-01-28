3807686b01cdd4d3bdd7421724fb36380cd102db
Shubham <shubhamjha3039@gmail.com>
2024-01-28 13:34:39 +0530

    importing files

---
+++ Missing_data.R
@@ -0,0 +1,14 @@
+#missing data
+x<-c(1,2,3,NA,5,6)
+x
+y<-is.na(x)
+x[!y]
+
+id<-c(1,2,3,4,5,6)
+temp<-c(25.8,34.2,54.3,NA,98.6,23.5)
+mydata<-data.frame(id,temp)
+mydata
+m<-is.na(mydata)
+m
+n<-complete.cases(mydata)
+n
---
+++ data_importing.R
@@ -0,0 +1,11 @@
+m<-read.table('dara.txt',header=TRUE)
+m
+
+mydata<-read.csv('data.csv',nrow=3,header=TRUE)
+mydata
+
+#data importing from internet
+url.show('link of the website')
+mydata<-read.table('link of the website')
+mydata
+
